425e4c12376e649ea1dabe720219dfa680f5f4e2
Praveen <Praveen@Praveen>
2022-02-17 13:53:16 +0530

    Synchronization

---
+++ src/Synchronization/ImplicitWait.java
@@ -0,0 +1,32 @@
+package Synchronization;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ImplicitWait {
+	static
+	{
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+	}
+
+	public static void main(String[] args)
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demo.actitime.com/login.do");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+		driver.findElement(By.id("username")).sendKeys("admin");
+		driver.findElement(By.name("pwd")).sendKeys("manager");
+	
+		driver.findElement(By.xpath("//div[text()='Login ']")).click();		
+		
+		
+		driver.findElement(By.id("container_reports")).click();
+		
+	}
+
+}
